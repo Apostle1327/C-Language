69ad2041da7214674234a0dbad20788b977396ed
Apostle1327 <apostle1327@gmail.com>
2024-01-10 21:08:12 +0530

    "Project 2 - Momemtum"

--- Lecture - 9 Ternary operators, Switch Case/Q2.C
+++ Lecture - 9 Ternary operators, Switch Case/Q2.C
@@ -14,7 +14,7 @@ main()
   int Language, Choice;
 
   clrscr();
-
+  Begin:
   printf("\nPress 1 to choose English");
   printf("\nPress 2 to choose Hindi");
   printf("\nPress 3 to choose Gujarati\n");
@@ -27,89 +27,93 @@ main()
 
   {
   case 1:
-    printf("\nEnglish Options");
-    printf("\n1 : Internet Pack Recharge");
-    printf("\n2 : Top-Up Pack Recharge");
-    printf("\n3 : Combo Pack Recharge");
-    printf("\n\nEnter Your Choice : ");
-    scanf("%d", &Choice);
+	printf("\nEnglish Options");
+	printf("\n1 : Internet Pack Recharge");
+	printf("\n2 : Top-Up Pack Recharge");
+	printf("\n3 : Combo Pack Recharge");
+	printf("\n\nEnter Your Choice : ");
+	scanf("%d", &Choice);
 
-    switch (Choice)
+	switch (Choice)
 
-    {
-    case 1:
-      printf("\nYou have Successfully recharged Your Internet pack");
-      break;
+	{
+	case 1:
+	  printf("\nYou have Successfully recharged Your Internet pack");
+	  break;
 
-    case 2:
-      printf("\nYou have Successfully recharged Your Top-Up pack");
-      break;
+	case 2:
+	  printf("\nYou have Successfully recharged Your Top-Up pack");
+	  break;
 
-    case 3:
-      printf("\nYou have Successfully recharged Your Combo pack");
-      break;
+	case 3:
+	  printf("\nYou have Successfully recharged Your Combo pack");
+	  break;
 
-    default:
-      printf("\nPlease enter a Correct value from the List");
-    }
-    break;
+	default:
+	  printf("\nPlease enter a Correct value from the List");
+	}
+	break;
 
   case 2:
-    printf("\nHindi Options");
-    printf("\n1 : Internet Pack Recharge");
-    printf("\n2 : Top-Up Pack Recharge");
-    printf("\n3 : Combo Pack Recharge");
-    printf("\n\nEnter Your Choice : ");
-    scanf("%d", &Choice);
+	printf("\nHindi Options");
+	printf("\n1 : Internet Pack Recharge");
+	printf("\n2 : Top-Up Pack Recharge");
+	printf("\n3 : Combo Pack Recharge");
+	printf("\n\nEnter Your Choice : ");
+	scanf("%d", &Choice);
 
-    switch (Choice)
+	switch (Choice)
 
-    {
-    case 1:
-      printf("\nAapne safaltapurvak apne Internet Pack ka recharge kar liya hai");
-      break;
+	{
+	case 1:
+	  printf("\nAapne safaltapurvak apne Internet Pack ka recharge kar liya hai");
+	  break;
 
-    case 2:
-      printf("\nAapne safaltapurvak apne Top-Up Pack ka recharge kar liya hai");
-      break;
+	case 2:
+	  printf("\nAapne safaltapurvak apne Top-Up Pack ka recharge kar liya hai");
+	  break;
 
-    case 3:
-      printf("\nAapne safaltapurvak apne Combo Pack ka recharge kar liya hai");
-      break;
+	case 3:
+	  printf("\nAapne safaltapurvak apne Combo Pack ka recharge kar liya hai");
+	  break;
 
-    default:
-      printf("\nKripya karke suchi mein se diye gaye sahi mulyon ka chunaav karein");
-    }
-    break;
+	default:
+	  printf("\nKripya karke suchi mein se diye gaye sahi mulyon ka chunaav karein");
+	}
+	break;
 
   case 3:
-    printf("\nGujarati Options");
-    printf("\n1 : Internet Pack Recharge");
-    printf("\n2 : Top-Up Pack Recharge");
-    printf("\n3 : Combo Pack Recharge");
-    printf("\n\nEnter Your Choice : ");
-    scanf("%d", &Choice);
+	printf("\nGujarati Options");
+	printf("\n1 : Internet Pack Recharge");
+	printf("\n2 : Top-Up Pack Recharge");
+	printf("\n3 : Combo Pack Recharge");
+	printf("\n\nEnter Your Choice : ");
+	scanf("%d", &Choice);
 
-    switch (Choice)
+	switch (Choice)
 
-    {
-    case 1:
-      printf("\nTamara Internet Pack nu safaltapurvak recharge thai gayu chhe");
-      break;
+	{
+	case 1:
+	  printf("\nTamara Internet Pack nu safaltapurvak recharge thai gayu chhe");
+	  break;
 
-    case 2:
-      printf("\nTamara Top-Up Pack nu safaltapurvak recharge thai gayu chhe");
-      break;
+	case 2:
+	  printf("\nTamara Top-Up Pack nu safaltapurvak recharge thai gayu chhe");
+	  break;
 
-    case 3:
-      printf("\nTamara Combo pack nu safaltapurvak recharge thai gayu chhe");
-      break;
+	case 3:
+	  printf("\nTamara Combo pack nu safaltapurvak recharge thai gayu chhe");
+	  break;
 
-    default:
-      printf("\nKrupa kari ne suchi ma aapva-ma aavel mulyo mathi saacha mulya ni pasandgi karo");
-    }
-    break;
+	default:
+	  printf("\nKrupa kari ne suchi ma aapva-ma aavel mulyo mathi saacha mulya ni pasandgi karo");
+	}
+	break;
+
+	default:
+	printf("\nPlease choose a Valid option\n");
+	goto Begin;
   }
 
-  getch();
+	getch();
 }
